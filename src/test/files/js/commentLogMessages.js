2fc829ba9af959529287041d79fb5cdac4741b43
CHAKROUN Anas <chakroun.anas@outlook.com>
2023-03-19 23:35:52 +0100

    test(comment-feature): test comment log messages. #204

---
+++ src/test/files/js/commentLogMessages.js
@@ -0,0 +1,19 @@
+const person = {
+    fullName: 'John Doe',
+    age: 25,
+    address: {
+        city: 'New York',
+        state: 'NY'
+    }
+}
+
+console.log("🚀 ~ person:", person);
+
+const isMarried = true;
+
+console.log("🚀 ~ isMarried:", isMarried);
+
+function sayHello(person) {
+    console.log("🚀 ~ sayHello ~ person:", person);
+    console.log(`Hello ${person.fullName}`)
+}
--- src/test/integration/js/commentLogMessages.test.ts
+++ src/test/integration/js/commentLogMessages.test.ts
@@ -1,31 +1,37 @@
+import Mocha from 'mocha';
+import * as assert from 'assert';
+import * as vscode from 'vscode';
+import { openDocument } from '../../helpers';
 
-import Mocha from "mocha";
-import * as assert from "assert";
-import * as vscode from "vscode";
-import { openDocument } from "../helpers";
+function wait(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
-test("Comment log messages inserted by the extension", async () => {
-  await openDocument("../files/js/commentLogMessages.js");
+test('Comment log messages inserted by the extension', async () => {
+  await openDocument('../files/js/commentLogMessages.js');
   const { activeTextEditor } = vscode.window;
   if (activeTextEditor) {
-    activeTextEditor.selections = [
-      new vscode.Selection(
-        new vscode.Position(0, 6),
-        new vscode.Position(0, 12)
-      ),
-    ];
     await vscode.commands.executeCommand(
-      "turboConsoleLog.displayLogMessage",
-      []
+      'turboConsoleLog.commentAllLogMessages',
+      [],
     );
+    await wait(250);
     const textDocument = activeTextEditor.document;
-    const logMessage = textDocument.lineAt(4).text;
-    assert.strictEqual(/console\.log\(.*/.test(logMessage), true);
+    const logMessagesLines = [9, 13, 16];
+    for (const logMessageLine of logMessagesLines) {
+      assert.strictEqual(
+        textDocument
+          .lineAt(logMessageLine)
+          .text.replace(/\s/g, '')
+          .startsWith('//'),
+        true,
+      );
+    }
   }
   Mocha.afterEach(async () => {
     await vscode.commands.executeCommand(
-      "workbench.action.closeActiveEditor",
-      []
+      'workbench.action.closeActiveEditor',
+      [],
     );
   });
 });
