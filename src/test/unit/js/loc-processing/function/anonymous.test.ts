72575c37212cfcaba67837a81ba05cdcc0d83da5
CHAKROUN Anas <chakroun.anas@outlook.com>
2023-04-13 19:06:12 +0000

    v2.9.2
    fix(loc-processing): argument of an anonymous function. #210
    feat(tests): script for running only unit tests.

--- src/test/unit/js/loc-processing/function/anonymous.test.ts
+++ src/test/unit/js/loc-processing/function/anonymous.test.ts
@@ -18,26 +18,42 @@ export default (): void => {
         ).to.equal(true);
       });
     });
-    it('Should return true if the LOC is an argument of an anonymous function', () => {
-      const anonymousFunctionsLOCs = [
-        'const sayHello = fullName => `Hello ${fullName}`',
-        'const happyBirthday = (fullName, age) => `Happy ${age} birthday ${fullName}`',
-        'fullName => `Hello ${fullName}`',
+    it('Should return true if the indicated parameter is an argument of the anonymous function', () => {
+      const anonymousFunctionsArgs = [
+        {
+          loc: 'const sayHello = fullName => `Hello ${fullName}`',
+          arg: 'fullName',
+        },
+        {
+          loc: 'const happyBirthday = (fullName, age) => `Happy ${age} birthday ${fullName}`',
+          arg: 'fullName',
+        },
+        {
+          loc: 'fullName => `Hello ${fullName}`',
+          arg: 'fullName',
+        },
+        {
+          loc: 'const user = users.find(item => item.email === email)',
+          arg: 'item',
+        },
       ];
-      anonymousFunctionsLOCs.forEach((anonymousFunctionLOC) => {
+      anonymousFunctionsArgs.forEach(({ loc, arg }) => {
         expect(
-          helpers.jsLineCodeProcessing.isArgumentOfAnonymousFunction(
-            anonymousFunctionLOC,
-            'fullName',
-          ),
+          helpers.jsLineCodeProcessing.isArgumentOfAnonymousFunction(loc, arg),
         ).to.equal(true);
       });
     });
-    it('Should return false if the LOC is not an argument of an anonymous function', () => {
+    it('Should return false if the indicated parameter is not an argument of the anonymous function', () => {
       expect(
         helpers.jsLineCodeProcessing.isArgumentOfAnonymousFunction(
           'function functionName(parameter){',
           'parameter',
+        ),
+      ).to.equal(false);
+      expect(
+        helpers.jsLineCodeProcessing.isArgumentOfAnonymousFunction(
+          'const user = users.find(item => item.email === email)',
+          'user',
         ),
       ).to.equal(false);
     });
